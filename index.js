de7bd2d916a7abd1246730dc102cbc7df0915dfc
brgjap11 <ross.graves11@gmail.com>
2018-05-22 15:44:22 -0400

    Final Work

--- index.js
+++ index.js
@@ -1,70 +1,140 @@
-//Get references to the tbody element, input fields and button
-var $tbody = document.querySelector("tbody");
-var $timeInput = document.querySelector("#date");
-var $cityInput = document.querySelector("#city");
-var $stateInput = document.querySelector("#state");
-var $countryInput = document.querySelector("#country");
-var $shapeInput = document.querySelector("#shape");
-var $searchBtn = document.querySelector("#search");
-var $loadMoreBtn = document.querySelector("#pagination");
-var startingIndex = 0;
-var resultsPerPage = 50;
-
+// Define references for the tbody element, search input fields and search button
+var $tbody = document.querySelector("tbody"); // Variable for table body in our HTML (for populating table in our HTML), this will be used in renderTable() function to 'populate' the table
+// Variable definitions for our 5 Search Fields in our HTML - Date/Time, City, State, Country and Shape
+var $DateTimeInput = document.querySelector("#Date"); // Variable for Date Time Input search category in our HTML, which will be used in our handleSearchButtonClick() function
+var $CityInput = document.querySelector("#City"); // Variable for City Input search category in our HTML, which will be used in our handleSearchButtonClick() function
+var $StateInput = document.querySelector("#State"); // Variable for State Input search category in our HTML, which will be used in our handleSearchButtonClick() function
+var $CountryInput = document.querySelector("#Country"); // Variable for Country Input search category in our HTML , which will be used in our handleSearchButtonClick() function
+var $ShapeInput = document.querySelector("#Shape"); // Variable for Shape Input search category in our HTML, which will be used in our handleSearchButtonClick() function
+// Search Button variable defintion
+var $searchBtn = document.querySelector("#search"); // Variable to call Search Button function in our HTML
 // Add an event listener to the searchButton, call handleSearchButtonClick when clicked
 $searchBtn.addEventListener("click", handleSearchButtonClick);
 
-// Set filteredAddresses to addressData initially
-var filteredDataSet = dataSet;
+// Add an event listener to initiate a search (by kicking off handleSearchButtonClick() function by just pressing the 'enter' key :)
+// Just a shortcut for quicker search results where you don't have to 'click' on the search button everytime...
+document.addEventListener('keypress', function (e) {
+  var key = e.which || e.keyCode;
+  if (key === 13) { // 13 is enter
+    handleSearchButtonClick(); // This is where we are calling the handleSearchButtonClick() function...
+  }
+});
 
-// renderTable renders dataset to the tbody
-function renderTable() {
+// Set Filtered Data to our source data, which is provided to us in our data.js file
+var filtered_data = dataSet;
+
+// Defining renderTable function. This function will be our main function to populate the table data. It will render the
+// filtered data (filtered_data) to our table body (tbody)
+function renderTable(filtered_data) { // function starts
   $tbody.innerHTML = "";
-  for (var i = 0; i < filteredDataSet.length; i++) {
-
-    // Get the current object and its fields
-    var data = filteredDataSet[i];
-    var fields = Object.keys(data);
-
+  for (var i = 0; i < filtered_data.length; i++) {
+    // Get get the current address object and its fields
+    var sighting = filtered_data[i];
+    var fields = Object.keys(sighting);
     // Create a new row in the tbody, set the index to be i + startingIndex
     var $row = $tbody.insertRow(i);
     for (var j = 0; j < fields.length; j++) {
-
-      // For every field in the table object, create a new cell at set its inner text to be the current value at the current field
+      // For every field in the address object, create a new cell at set its inner text to be the current value at the current address's field
       var field = fields[j];
       var $cell = $row.insertCell(j);
-      $cell.innerText = data[field];
+      $cell.innerText = sighting[field];
     }
   }
 }
 
-function handleSearchButtonClick() {
+function handleSearchButtonClick() // function starts
+{
+// for 'comments' in the source data.js file are in 'LOWER CASE'
 
-  // Format the user's search by removing leading and trailing whitespace, lowercase the string
-  var filterDateTime = $timeInput.value.trim().toLowerCase();
-  var filterCity = $cityInput.value.trim().toLowerCase();
-  var filterCountry = $countryInput.value.trim().toLowerCase();
-  var filterState = $stateInput.value.trim().toLowerCase();
-  var filterShape = $shapeInput.value.trim().toLowerCase();
+  var filterDateTime = $DateTimeInput.value.trim().toLowerCase();  
+  var filterCity = $CityInput.value.trim().toLowerCase();
+  var filterState = $StateInput.value.trim().toLowerCase();
+  var filterCountry = $CountryInput.value.trim().toLowerCase();
+  var filterShape = $ShapeInput.value.trim().toLowerCase();
+  
+// Defining our first IF Condition - This condition will kick off if a user enters 'any value' in any 'one' of our 5 search fields
+// Our 5 Search Fields in our HTML are - Date/Time, City, State, Country and Shape
+  if (filterDateTime || filterCity || filterState || filterCountry || filterShape)
+  {
+    // Code logic for filtering the table contents based on data being input on the Date/Time field
+    if (filterDateTime){ 
+      //Set filtered_data to an array of all table enteries whose "Date/Time" matches the filter
+      filtered_data = dataSet.filter (function(sighting) { 
+        var SightingDateTime = sighting.datetime.toLowerCase();
+        // If true, add the corresponding table data to the filtered_data, otherwise don't add it to filtered_data
+        return SightingDateTime === filterDateTime;
+      });
+    } else {filtered_data = dataSet}; // return initial dataSet if "DateTime" Search Field is blank
+    
+    // Code logic for filtering the table contents based on data being input on the City field
+    if (filterCity){
+      //Set filtered_data to an array of all table enteries whose "City" matches the filter
+      filtered_data = filtered_data.filter (function(sighting) {
+        var SightingCity = sighting.city.toLowerCase();
+        // If true, add the corresponding table data to the filtered_data, otherwise don't add it to filtered_data
+        return SightingCity === filterCity;
+      });
+    } else {filtered_data = filtered_data}; // return initial filtered_data dataSet if "City" search field is blank
 
-  // Set filteredDataSet to an array of all data whose fields matches the filter
-  filteredDataSet = dataSet.filter(function(data) {
-    var dateTimeField = data.datetime.toLowerCase();
-    var cityField = data.city.toLowerCase();
-    var stateField = data.state.toLowerCase();
-    var countryField = data.country.toLowerCase();
-    var shapeField = data.shape.toLowerCase();
+    // Code logic for filtering the table contents based on data being input on the State field
+    if (filterState){
+      //Set filtered_data to an array of all table enteries whose "State" matches the filter
+      filtered_data = filtered_data.filter (function(sighting) {
+        var SightingState = sighting.state.toLowerCase();
+        // If true, add the corresponding table data to the filtered_data, otherwise don't add it to filtered_data      
+        return SightingState === filterState;
+      });
+    } else {filtered_data = filtered_data}; // return initial filtered_data dataSet if "State" search field is blank
 
-    var allFields = 
-      (filterDateTime === "" || dateTimeField === filterDateTime) &&
-      (filterCity === "" || cityField === filterCity) &&
-      (filterCountry === "" || countryField === filterCountry) &&
-      (filterState === "" || stateField === filterState) &&
-      (filterShape === "" || shapeField === filterShape);
-    return allFields;
+    // Code logic for filtering the table contents based on data being input on the Country field
+    if (filterCountry){
+      //Set filtered_data to an array of all table enteries whose "Country" matches the filter
+      filtered_data = filtered_data.filter (function(sighting) {
+        var SightingCountry = sighting.country.toLowerCase();
+        // If true, add the corresponding table data to the filtered_data, otherwise don't add it to filtered_data        
+        return SightingCountry === filterCountry;
+      });
+    } else {filtered_data = filtered_data}; // return initial filtered_data dataSet if "Country" search field is blank
 
-  });
-  renderTable();
+    // Code logic for filtering the table contents based on data being input on the Shape field
+    if (filterShape){
+      //Set filtered_data to an array of all table enteries whose "Shape" matches the filter      
+      filtered_data = filtered_data.filter (function(sighting) {
+        var SightingShape = sighting.shape.toLowerCase();
+        // If true, add the corresponding table data to the filtered_data, otherwise don't add it to filtered_data        
+        return SightingShape === filterShape;
+      });
+    } else {filtered_data = filtered_data}; // return initial filtered_data dataSet if "Shape" seach field is blank
+
+
+ } else {
+  //location.reload(); //Commented this code out as we don't need to refresh the web page if the user doesn't select any inputs in the search fields
+  // Earlier I was refreshing the web page, but it is bad for performance. Instead I am now rendering 'full table' when user doesn't put any value in the search fields
+  filtered_data = dataSet; // This is where we render full table, i.e., we re-initialize our table (complete data set) if all search enteries are blank
+  }
+  $('#table').DataTable().destroy(); // Destroy the already initialized earlier version of the pagination table
+  renderTable(filtered_data); // Call the rederTable() function to render (display) the table with our filtered dataset (filtered_data)
+  tushaar_pagination(); // Re-initate the pagination table with the new rendered search results
 }
 
 
+function pagination() 
+{
+  $(document).ready(function() {
+    if ( $.fn.dataTable.isDataTable( '#table' ) ) { // This conditions checks for table being active and bypasses an active table - this is fail safe to remove warning errors
+      $('#table').DataTable();
+    }
+    else {
+      $('#table').DataTable({
+        "pagingType": "full_numbers",
+        destroy: true, // table needs to be destroyed after one active session
+        paging: true, // this parameter sets the table paging to be active
+        searching: false // this parameter removes the 'search' box, which is optional but I removed it because it may otherwise confuse the user
+        });
+        } 
+      });
+}
 
+
+renderTable(filtered_data);
+pagination(); // Initiate the pagination function for the first time the browser page loads
